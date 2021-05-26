58e232245efc1513ad6fd5601d4ef72e47220cb4
Joseph <josephsasson1@gmail.com>
2021-05-26 14:26:22 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+/*
+for ([initialization]; [condition]; [iteration]) {
+  [loop body]
+} 
+*/
+
+function writeCards(cards, event){
+  const newArray = []
+    for (let i = 0; i < cards.length; i++ ){
+      newArray.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+  }
+  return newArray
+}
+
+/*
+while ([condition]) {
+  [loop body]
+} 
+*/
+
+function countDown(){
+  let newCount = 10
+  while (newCount >-1) {
+    console.log (newCount--)
+  }
+}
